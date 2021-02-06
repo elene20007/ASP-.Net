c7970e64d681a39ba2354a976440f918710a3287
elene20007 <elene.akhalkatsi.1@btu.edu.ge>
2021-02-07 03:06:12 +0400

    01-types

---
+++ day-4/01-types/Practices/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Practices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////// Practice1 - Sum of numbers /////
+            Console.Write("number one: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("number two: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            int result = firstNumber + secondNumber;
+            Console.WriteLine("result: " + result);
+
+
+
+            //////Practice2 - multiplication of two nums ////
+            Console.Write("Input first number: ");
+            double firstNum = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input second number: ");
+            double secondNum = Convert.ToDouble(Console.ReadLine());
+            double resultt = firstNum * secondNum;
+            bool isGreater = false;
+            if (resultt > 10)
+            {
+                isGreater = true;
+            }
+            Console.WriteLine("Result: " + resultt + "\nResult is greater than 10: " + isGreater);
+
+
+
+            ////////Practice3 - Equals or not  //////
+            Console.Write("Input first number: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input second number: ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+            bool isEqual = false;
+            if (firstNumber == secondNumber)
+            {
+                isEqual = true;
+            }
+            Console.WriteLine("Numbers are equal: " + isEqual);
+
+
+
+            ////////// Practice 4 - double to int /////
+            Console.Write("Input double number: ");
+            int num = Convert.ToInt32(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Result: " + num);
+
+
+
+            ///// Practice 5 -  string characters /////
+            Console.Write("Input string: ");
+            string text = Console.ReadLine();
+            Console.WriteLine(text.Substring(0, 4));
+
+
+            //// Practice 6 - leap year //////
+            Console.Write("Input year number: ");
+            double inputYear = Convert.ToDouble(Console.ReadLine());
+            bool isLeap = false;
+
+            if (inputYear % 4 == 0)
+            {
+                isLeap = true;
+            }
+            Console.WriteLine($"{inputYear} is leap year: " + isLeap);
+        }
+    }
+}
---
+++ day-4/01-types/Tutorials/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Tutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //////Tutorial1///////////
+       
+            Console.Write("Input first number: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input second number: ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+            double result = firstNumber + secondNumber;
+            bool isEven = false;
+
+            if (result%2 == 0)
+            {
+                isEven = true;
+            }
+
+            Console.WriteLine("Result: " + Convert.ToString(result) + "\n" + "Result is even number: " + Convert.ToString(isEven));
+            
+
+            //////Tutorial2//////////
+
+            DateTime date = DateTime.Now;
+            string current = date.ToString("dddd, dd MMMM yyyy");
+            string result = current.Remove(8,12);
+            Console.WriteLine("Current date: " + current + "\n" + "Result: " + result);
+        }
+    }
+}
